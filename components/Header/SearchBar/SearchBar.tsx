39ce44f025c3f2c3f345520c406e73ff3e9215c1
Rocamain <fjrocavazquez@gmail.com>
2024-03-06 01:07:45 +0000

    initial implementation of PlacesAutoComplete component, and modify searchBar and SearchInput

--- components/Header/SearchBar/SearchBar.tsx
+++ components/Header/SearchBar/SearchBar.tsx
@@ -1,38 +1,36 @@
 'use client'
 import { WhiteMagnifier } from '@/components/Icons'
 import SearchInput from './SearchInput'
+import PlacesAutoCompleteInput from './PlacesAutoCompleteInput'
 
-interface SearBarProps {
+interface SearchBarProps {
   mobile?: boolean
 }
 
-export default function SearchBar({ mobile = false }: SearBarProps) {
+export default function SearchBar({ mobile = false }: SearchBarProps) {
   // PENDING IMPLEMENTATION
   // const searchByWords = async (formData: FormData) => {
   //   'use server'
   // }
-  const handleClick = () => {
-    console.log('clicked')
-  }
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
+
   const isMobile = mobile ? 'mt-10 mx-auto lg:hidden' : 'hidden'
 
   return (
     <>
       <form
-        // PENDING IMPLEMENTATION
-        // action={searchByWords}
-        className={`grow max-w-xl flex flex-row items-center justify-center lg:flex lg:flex-row lg:items-center ${isMobile}`}
+        className={`realtive grow max-w-xl flex flex-row items-center justify-center lg:flex lg:flex-row lg:items-center ${isMobile}`}
+        onSubmit={(event) => event.preventDefault()}
       >
         <SearchInput />
+        <PlacesAutoCompleteInput />
         <div className="flex flex-row items-center justify-between bg-red-500 rounded-r-lg p-3 border border-red-400 hover:bg-red-500 focus:z-10 peer-focus:border-red-400">
-          {/*PENDING IMPLEMENTATION*/}
-          {/* <SearchAutoComplete/> */}
-
           <button
             type="submit"
             className="flex-col items-center justify-center"
             aria-label="Search events"
-            onClick={handleClick}
           >
             <WhiteMagnifier />
           </button>
--- components/Header/SearchBar/SearchInput.tsx
+++ components/Header/SearchBar/SearchInput.tsx
@@ -3,8 +3,9 @@ export default function SearchInput() {
     <input
       name="searchText"
       type="text"
+      autoComplete="off"
       placeholder="Search for events"
-      className="peer outline-none appearance-none text-md text-gray-600 p-3 pl-4 rounded-l-lg border border-gray-400  hover:border-gray-300 focus:border-red-400 hover:z-10 focus:z-10 flex-grow w-full rounded-r-none placeholder:text-gray-500"
+      className="flex-grow w-1/2 outline-none appearance-none text-md text-gray-600 p-3 pl-4 rounded-l-lg border border-gray-400  hover:border-gray-300 focus:border-red-400 hover:z-10 focus:z-10  rounded-r-none placeholder:text-gray-500"
     />
   )
 }
