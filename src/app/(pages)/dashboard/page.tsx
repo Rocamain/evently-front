07bd7b0f93c8be819c449cd89213262d55875475
Rocamain <fjrocavazquez@gmail.com>
2024-05-23 00:13:43 +0100

    Creates new routes

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashBoardPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Eventy Dashboard</h1>
+    </main>
+  )
+}
