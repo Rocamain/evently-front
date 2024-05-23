e834b71cb0aac61b5408aa9307844cd97253f387
Rocamain <fjrocavazquez@gmail.com>
2024-05-24 00:07:23 +0100

    Misspel

--- src/app/(pages)/(auth)/signIn/layout.tsx
+++ src/app/(pages)/(auth)/signIn/layout.tsx
@@ -3,7 +3,7 @@ export const metadata = {
   title: 'Evently SignIn',
   description: 'Evently signIn to your account',
 }
-export default function LayoutSingInPage({
+export default function LayoutSignInPage({
   children,
 }: {
   children: ReactNode
