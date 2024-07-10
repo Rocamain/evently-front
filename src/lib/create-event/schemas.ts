824747578712e868d5adfe2a906cd692a80f5ca2
Rocamain <fjrocavazquez@gmail.com>
2024-07-10 20:22:10 +0100

    Redefine schema and add new elements

--- src/lib/create-event/schemas.ts
+++ src/lib/create-event/schemas.ts
@@ -1,31 +1,46 @@
 import { z } from 'zod'
 
-const AddressComponentSchema = z.object({
-  long_name: z.string(),
-  short_name: z.string(),
-  types: z.array(z.string()),
-})
-
-const EventLocationSchema = z.object({
-  id: z.string(),
-  name: z.string().optional(),
-  address: z.array(AddressComponentSchema).optional(),
-  lat: z.number().optional(),
-  lng: z.number().optional(),
-})
-
+// Define the FileArraySchema if not defined already
 const FileArraySchema = z.array(z.instanceof(File))
 
+// Define the schema for the event
 export const CreateEventSchema = z.object({
   eventTitle: z.string().min(1, 'Event title is required'),
   eventLink: z.string().url('Invalid URL'),
-  eventLocation: EventLocationSchema.or(z.literal('')),
+  eventLocationId: z.string().min(1, 'Event location ID is required'),
+  eventLocationAddress: z.string().min(1, 'Event location address is required'),
+  eventLocationLat: z
+    .number()
+    .min(-90, 'Invalid latitude')
+    .max(90, 'Invalid latitude'),
+  eventLocationLng: z
+    .number()
+    .min(-180, 'Invalid longitude')
+    .max(180, 'Invalid longitude'),
+  eventGeoHash: z.string(),
   eventCategory: z.string().min(1, 'Event category is required'),
   eventPrice: z.number().min(0, 'Event price must be a positive number'),
   eventTime: z
     .string()
     .regex(/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/, 'Invalid time format'),
-  eventDate: z.date(),
+  eventDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format'), // Example date format YYYY-MM-DD
   eventDescription: z.string().min(1, 'Event description is required'),
-  eventPictures: FileArraySchema,
+  files: FileArraySchema,
 })
+
+// Define the interface
+export interface CreateEventState {
+  eventTitle: string
+  eventLink: string
+  eventLocationName: string
+  eventLocationId: string
+  eventLocationAddress: string
+  eventLocationLat: number
+  eventLocationLng: number
+  eventCategory: string
+  eventPrice: number
+  eventTime: string
+  eventDate: string
+  eventDescription: string
+  files: File[]
+}
