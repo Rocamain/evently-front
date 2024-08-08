c05bcf5d63217dc7afd15af81cf14e1d8c45c11e
Rocamain <fjrocavazquez@gmail.com>
2024-08-08 11:00:32 +0100

    Attendees card now fixed

--- src/ui/event-by-id/EventAttendees/EventBookingAttendees.tsx
+++ src/ui/event-by-id/EventAttendees/EventBookingAttendees.tsx
@@ -1,6 +1,7 @@
 import { Attendee, Booking } from '@/types/event/event'
 import { AttendeeCard } from './EventAttendeeCard'
 import Image from 'next/image'
+import EventRestOfAttendeesCard from './EventRestOfAttendeesCard'
 interface EventBookingAttendeesProps {
   attendees: Attendee[]
 }
@@ -15,21 +16,24 @@ export default function EventBookingAttendees({
   const firstAttendees = attendees?.slice(0, 2)
   const restAttendees = attendees?.slice(2)
 
-  // return <div> EventBookingAttendees</div>
   return (
     <>
       {firstAttendees &&
         firstAttendees.map((attendee, index) => (
           <AttendeeCard key={`attendees-link-${index}`} attendee={attendee} />
         ))}
-      {restAttendees &&
+
+      {restAttendees && (
+        <EventRestOfAttendeesCard restOfAttendees={restAttendees} />
+      )}
+      {/* {restAttendees &&
         restAttendees.map((attendee, index) => (
           <AttendeeCard
             key={`attendees-link-rest-${index}`}
             attendee={attendee}
           />
-        ))}
-      {restAttendees && restAttendees.length > 1 && (
+        ))} */}
+      {/* {restAttendees && restAttendees.length > 1 && (
         <a href={`/event/${eventId}/attendees/`} className="hover:no-underline">
           <div className="flex flex-col bg-white rounded-md py-5 px-2 items-center relative shadow-[0px_0px_5px_0px_rgba(0,0,0,0.12)] h-[182px] max-w[150px] min-w-[125px]">
             <div className="relative">
@@ -39,7 +43,7 @@ export default function EventBookingAttendees({
 
               <ul className="flex flex-row">
                 {restAttendees &&
-                  restAttendees.slice(0, 2).map(({ userPicture }, index) => (
+                  restAttendees.slice(0, 1).map(({ userPicture }, index) => (
                     <li
                       key={`attendee-rest-${index}`}
                       className="mr-[-50px] z-0"
@@ -65,7 +69,7 @@ export default function EventBookingAttendees({
             </span>
           </div>
         </a>
-      )}
+      )} */}
     </>
   )
 }
---
+++ src/ui/event-by-id/EventAttendees/EventRestOfAttendeesCard.tsx
@@ -0,0 +1,54 @@
+import { Attendee } from '@/types/event/event'
+import Image from 'next/image'
+
+interface RestOfAttendeeProps {
+  restOfAttendees: Array<Attendee>
+}
+
+export default function EventRestOfAttendeesCard({
+  restOfAttendees,
+}: {
+  restOfAttendees: Attendee[]
+}) {
+  const totalRestOfAttendees = restOfAttendees.length
+  const totalShowPhotos = restOfAttendees.slice(0, 3)
+
+  return (
+    <div className=" bg-white rounded-md py-5 px-2  shadow-[0px_0px_5px_0px_rgba(0,0,0,0.12)] h-[182px] max-w[120px] min-w-[105px]">
+      <div className="flex items-start justify-center relative">
+        {totalShowPhotos.map(({ userPicture, userName }, index) => {
+          const isLast = index + 1 === totalShowPhotos.length
+
+          const a = 100
+          return (
+            <div
+              key={index}
+              className={`rekative z-0 ${isLast ? '' : 'mr-[-50px]'}`}
+            >
+              <Image
+                alt={`photo of user ${userName}`}
+                width={72}
+                height={72}
+                src={
+                  userPicture
+                    ? userPicture
+                    : '/images/jimmy-dean-my1mDMraGf0-unsplash.jpg'
+                }
+                loading="eager"
+                className="rounded-full border-2 border-gray-300 object-cover min-h-[72px] min-w-[72px]"
+              />
+              {isLast && (
+                <div className="absolute left-[50%] bottom-[35%] z-10 text-xl font-bold text-white">
+                  +2
+                </div>
+              )}
+            </div>
+          )
+        })}
+      </div>
+      <h3 className="whitespace-pre-wrap text-teal-600 text-center font-semibold text-sm mt-3 line-clamp-2">
+        {`+${totalRestOfAttendees} more`}
+      </h3>
+    </div>
+  )
+}
