03c17f8c54897e4e5207461040acd5bead1d2a7c
Rocamain <fjrocavazquez@gmail.com>
2024-08-08 10:54:55 +0100

    Modifies Auth wrapper and types

--- src/ui/auth/AuthWrapper.tsx
+++ src/ui/auth/AuthWrapper.tsx
@@ -8,13 +8,13 @@ export default function AuthWrapper({
   return (
     <div className="overflow-visible flex items-center justify-center">
       <div className="flex flex-col justify-center">
-        <div className="relative sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-teal-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative mx-5 max-w-md sm:max-w-lg md:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-teal-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-2xl sm:rounded-3xl"></div>
           <div
-            className="relative px-6 py-8 bg-white shadow-lg sm:rounded-3xl sm:p-20 sm:pt-10"
+            className="relative px-12 py-8 bg-white shadow-lg rounded-2xl sm:rounded-3xl sm:p-20 sm:pt-10"
             style={{ background: '#edf2f7' }}
           >
-            <div className="pt-4 sm:pt- max-w-md mx-auto">{children}</div>
+            <div className="md:pt-4 sm:pt- max-w-md mx-auto">{children}</div>
           </div>
         </div>
       </div>
