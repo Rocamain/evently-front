cbb7b497b84f7c387e4864716317f0fe66fe9d6c
Rocamain <fjrocavazquez@gmail.com>
2024-08-08 10:45:59 +0100

    Creates new pages

---
+++ src/app/(pages)/booking/[bookingId]/cancel/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function CancelBookingPage() {
+  return <div>page</div>
+}
---
+++ src/app/(pages)/event/[eventId]/cancel/page.tsx
@@ -0,0 +1,3 @@
+export default function CancelEventPage() {
+  return <div>CancelEventPage</div>
+}
