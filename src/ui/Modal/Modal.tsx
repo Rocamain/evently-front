d13e05ed22c1a8488ce7e77cb791c1a1fc2b5d44
Rocamain <fjrocavazquez@gmail.com>
2024-08-17 20:54:42 +0100

    Modifies modal

--- src/ui/Modal/Modal.tsx
+++ src/ui/Modal/Modal.tsx
@@ -1,73 +1,9 @@
 'use client'
-import React, {
-  type ElementRef,
-  useCallback,
-  useRef,
-  useEffect,
-  MouseEventHandler,
-} from 'react'
-
-import { useRouter } from 'next/navigation'
+import React from 'react'
+import useModal from '@/app/hooks/useModal'
 
 export default function Modal({ children }: { children: React.ReactNode }) {
-  const dialogRef = useRef<ElementRef<'dialog'>>(null)
-  const buttonRef = useRef<ElementRef<'button'>>(null)
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!dialogRef.current?.open) {
-      dialogRef.current?.showModal()
-    }
-  }, [])
-
-  const onDismiss = useCallback(() => {
-    router.back()
-  }, [router])
-
-  const handleClick: MouseEventHandler = useCallback(
-    (e) => {
-      if (onDismiss) {
-        if (dialogRef.current) {
-          const dialogDimensions = dialogRef.current.getBoundingClientRect()
-          // if click outside the dialog  then close it.
-          if (
-            e.clientX < dialogDimensions.left ||
-            e.clientX > dialogDimensions.right ||
-            e.clientY < dialogDimensions.top ||
-            e.clientY > dialogDimensions.bottom
-          ) {
-            dialogRef.current.close()
-          }
-        }
-
-        if (e.target === buttonRef.current && dialogRef.current?.open) {
-          onDismiss()
-        }
-      }
-    },
-    [onDismiss, dialogRef, buttonRef],
-  )
-
-  const onKeyDown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === 'Escape') onDismiss()
-    },
-    [onDismiss],
-  )
-
-  // Lock scroll when component mounts
-  useEffect(() => {
-    document.body.style.overflow = 'hidden'
-    return () => {
-      document.body.style.overflow = 'auto'
-    }
-  }, [])
-
-  useEffect(() => {
-    document.addEventListener('keydown', onKeyDown)
-    return () => document.removeEventListener('keydown', onKeyDown)
-  }, [onKeyDown])
-
+  const { dialogRef, buttonRef, onDismiss, handleClick } = useModal()
   return (
     <dialog
       ref={dialogRef}
@@ -75,7 +11,7 @@ export default function Modal({ children }: { children: React.ReactNode }) {
       onClose={onDismiss}
       onClick={handleClick}
     >
-      <div className="absolute top-4 right-[20px] z-50">
+      <div className="absolute top-8 right-10 z-50">
         <button ref={buttonRef} className="text-xl text-gray-500">
           ✖
         </button>
