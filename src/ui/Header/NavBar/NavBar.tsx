fac165c3cd217626478e1318f2eea374a62248fd
Rocamain <fjrocavazquez@gmail.com>
2024-11-20 15:56:01 +0000

    Modifies Header and Navbar component

--- src/ui/Header/NavBar/NavBar.tsx
+++ src/ui/Header/NavBar/NavBar.tsx
@@ -1,12 +1,13 @@
 import LinkButton from '@/ui/buttons/LinkButton/LinkButton'
-import { verifySession } from '@/lib/auth/session'
 import { logout } from '@/lib/auth/action'
 import SubmitButton from '@/ui/buttons/SubmitButton/SubmitButton'
 import MenuButton from '../Menu/MenuButton'
 
-export default async function NavBar() {
-  const isVerifiedSession = await verifySession()
-
+export default async function NavBar({
+  isVerifiedSession,
+}: {
+  isVerifiedSession: Boolean
+}) {
   return (
     <nav className="flex-grow-0">
       <ul className=" flex justify-end md:space-x-4 items-center">
